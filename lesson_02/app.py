0b9ade8a64a5f293d00d7c0cf11217d0343d8ee8
Denis Malich <denis.malich@mail.ru>
2024-07-08 13:39:34 +0400

    Разрешены конфликты после объединения с удалённым репозиторием

--- lesson_02/app.py
+++ lesson_02/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 
 @app.route('/')
@@ -8,29 +9,36 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/set_cookie', methods=['POST'])
-def set_cookie():
-    name = request.form['name']
+@app.route('/category/<cat>')
+def category(cat):
+    # Здесь можно обрабатывать разные категории товаров
+    return render_template('category.html', category=cat)
+
+
+@app.route('/setcookie', methods=['POST'])
+def setcookie():
+    user = request.form['nm']
     email = request.form['email']
-    resp = make_response(redirect(url_for('welcome')))
-    resp.set_cookie('name', name)
-    resp.set_cookie('email', email)
+    resp = make_response(redirect(url_for('greeting')))
+    resp.set_cookie('userName', user)
+    resp.set_cookie('userEmail', email)
     return resp
 
 
-@app.route('/welcome')
-def welcome():
-    name = request.cookies.get('name')
-    if not name:
+@app.route('/greeting')
+def greeting():
+    user = request.cookies.get('userName')
+    email = request.cookies.get('userEmail')
+    if not user or not email:
         return redirect(url_for('index'))
-    return render_template('welcome.html', name=name)
+    return render_template('greeting.html', user=user)
 
 
 @app.route('/logout')
 def logout():
     resp = make_response(redirect(url_for('index')))
-    resp.delete_cookie('name')
-    resp.delete_cookie('email')
+    resp.set_cookie('userName', '', expires=0)
+    resp.set_cookie('userEmail', '', expires=0)
     return resp
 
 
